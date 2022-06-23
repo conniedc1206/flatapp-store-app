9b3a7554dc0851bbb57add1dd49356631646ea57
Connie Park <conniedc1206@gmail.com>
2022-06-23 12:01:02 -0700

    Started cart and cart item components, connected to add to cart button in items details

--- flatapp-store/src/components/NavBar.js
+++ flatapp-store/src/components/NavBar.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../style.css";
 import flatapplogo from "../flatapplogo.png";
+import { BsCart3 } from "react-icons/bs";
 
 function NavBar() {
   return (
     <nav>
-      <img src={flatapplogo} className="nav--icon" />
-      <h1>FlatApp</h1>
+      <Link exact to="/">
+        <img src={flatapplogo} alt="flatapp logo" className="nav--icon" />
+        <h1>FlatApp</h1>
+      </Link>
+
       <Link exact to="/">
         Store
       </Link>
@@ -15,6 +19,10 @@ function NavBar() {
       <Link to="/seller">Seller</Link>
       <Link to="/blogs">Blogs</Link>
       <Link to="/login">Login</Link>
+      <br></br>
+      <Link to="/cart">
+        <BsCart3 />
+      </Link>
     </nav>
   );
 }
